8c428ad4d06474689ae7ddbfa554a029497294d8
hustlenut <hustlenut@protonmail.com>
2024-03-11 19:43:45 +0100

    added templ format

--- init.lua
+++ init.lua
@@ -576,6 +576,24 @@ require('lazy').setup({
         },
       }
 
+      -- Function to format .templ files using templ fmt
+      local templ_format = function()
+        local bufnr = vim.api.nvim_get_current_buf()
+        local filename = vim.api.nvim_buf_get_name(bufnr)
+        local cmd = 'templ fmt ' .. vim.fn.shellescape(filename)
+
+        vim.fn.jobstart(cmd, {
+          on_exit = function()
+            if vim.api.nvim_get_current_buf() == bufnr then
+              vim.cmd 'e!'
+            end
+          end,
+        })
+      end
+
+      -- Auto-format on save for .templ files
+      vim.api.nvim_create_autocmd({ 'BufWritePre' }, { pattern = { '*.templ' }, callback = templ_format })
+
       -- Ensure the servers and tools above are installed
       --  To check the current status of installed tools and/or manually install
       --  other tools, you can run
